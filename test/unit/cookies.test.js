14f23c51be53e8f773bff9ac9fdc7cade9b2f229
Satish Chatap <satish.chatap@capgemini.com>
2022-06-14 21:04:25 +0100

    crumb and session test

--- test/unit/cookies.test.js
+++ test/unit/cookies.test.js
@@ -1,3 +1,4 @@
+const { cookie: { cookieNameCookiePolicy } } = require('../../app/config')
 const cookies = require('../../app/cookies')
 let request
 let h
@@ -7,8 +8,7 @@ describe('cookies', () => {
   beforeEach(() => {
     request = {
       state: {
-        cookies_policy: undefined,
-        session: true
+        [cookieNameCookiePolicy]: undefined
       }
     }
     h = {
@@ -22,18 +22,24 @@ describe('cookies', () => {
 
   test('getCurrentPolicy returns default cookie if does not exist', () => {
     const result = cookies.getCurrentPolicy(request, h)
-    expect(result).toEqual(defaultCookie)
+    expect(result).toStrictEqual(defaultCookie)
   })
+
+  test('getCurrentPolicy sets default cookie if does not exist', () => {
+    cookies.getCurrentPolicy(request, h)
+    expect(h.state).toHaveBeenCalledWith(cookieNameCookiePolicy, defaultCookie)
+  })
+
   test('getCurrentPolicy returns cookie if exists', () => {
-    request.state.cookies_policy = { confirmed: true, essential: false, analytics: true }
+    request.state[cookieNameCookiePolicy] = { confirmed: true, essential: false, analytics: true }
     const result = cookies.getCurrentPolicy(request, h)
-    expect(result).toStrictEqual({ confirmed: false, essential: true, analytics: false })
+    expect(result).toStrictEqual({ confirmed: true, essential: false, analytics: true })
   })
 
   test('getCurrentPolicy does not set default cookie if exists', () => {
-    request.state.cookies_policy = { confirmed: true, essential: true, analytics: true }
+    request.state[cookieNameCookiePolicy] = { confirmed: true, essential: false, analytics: true }
     cookies.getCurrentPolicy(request, h)
-    expect(h.state).toHaveBeenCalled()
+    expect(h.state).not.toHaveBeenCalled()
   })
 
   test('updatePolicy sets cookie twice if does not exist', () => {
@@ -43,19 +49,18 @@ describe('cookies', () => {
 
   test('updatePolicy sets confirmed cookie second if does not exist', () => {
     cookies.updatePolicy(request, h, true)
-
-    expect(h.state).toHaveBeenCalled()
+    expect(h.state).toHaveBeenNthCalledWith(2, cookieNameCookiePolicy, { confirmed: true, essential: true, analytics: true })
   })
 
   test('updatePolicy sets cookie to accepted', () => {
-    request.state.cookies_policy = { confirmed: false, essential: true, analytics: false }
+    request.state[cookieNameCookiePolicy] = { confirmed: false, essential: true, analytics: false }
     cookies.updatePolicy(request, h, true)
-    expect(h.state).toHaveBeenCalled()
+    expect(h.state).toHaveBeenCalledWith(cookieNameCookiePolicy, { confirmed: true, essential: true, analytics: true })
   })
 
   test('updatePolicy sets cookie to rejected', () => {
-    request.state.cookies_policy = { confirmed: false, essential: true, analytics: false }
+    request.state[cookieNameCookiePolicy] = { confirmed: false, essential: true, analytics: false }
     cookies.updatePolicy(request, h, false)
-    expect(h.state).toHaveBeenCalled()
+    expect(h.state).toHaveBeenCalledWith(cookieNameCookiePolicy, { confirmed: true, essential: true, analytics: false })
   })
 })
