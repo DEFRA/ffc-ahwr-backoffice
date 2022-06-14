abe1b93951a6928bd76845ef1843e78230b34752
Steve Hunt <st3v3nhunt@users.noreply.github.com>
2022-06-14 09:07:55 +0100

    feat: set pino pretty print for logs

--- app/plugins/logging.js
+++ app/plugins/logging.js
@@ -1,14 +1,11 @@
+const config = require('../config')
+
 module.exports = {
   plugin: require('hapi-pino'),
   options: {
     level: 'warn',
     logPayload: true,
     logQueryParams: true,
-    transport: {
-      target: 'pino-pretty',
-      options: {
-        colorize: true
-      }
-    }
+    prettyPrint: config.isDev
   }
 }
