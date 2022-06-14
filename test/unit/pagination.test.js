cf3c5358bdbb3aec1371d8ba1276adaa3091e9cb
Shivanshu Patel <shivanshuit914@gmail.com>
2022-06-14 21:21:31 +0100

    feat: add unit tests for session and pagination

---
+++ test/unit/pagination.test.js
@@ -0,0 +1,17 @@
+const pagination = require('../../app/pagination')
+
+describe('Pagination', () => {
+  test('getPagination test', () => {
+    const result = pagination.getPagination(100, 10)
+    expect(result.limit).toBe(10)
+    expect(result.offset).toBe(990)
+  })
+  test('getPagingData test', () => {
+    const result = pagination.getPagingData(100, 10, 1, 'test.com')
+    expect(result.page).toBe(1)
+    expect(result.limit).toBe(10)
+    expect(result.totalPages).toBe(10)
+    expect(result.total).toBe(100)
+    expect(result.url).toBe('test.com')
+  })
+})
