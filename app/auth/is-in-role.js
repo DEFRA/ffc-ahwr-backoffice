d074b95d9c86d92430ff259f3738d2581965e65e
Shivanshu Patel <shivanshuit914@gmail.com>
2022-06-16 20:26:13 +0100

    fix: linting

--- app/auth/is-in-role.js
+++ app/auth/is-in-role.js
@@ -1,8 +1,8 @@
 const isInRole = (credentials, role) => {
-	if (!credentials || !credentials.scope) {
-		return false
-	}
-	return credentials.scope.includes(role)
+  if (!credentials || !credentials.scope) {
+    return false
+  }
+  return credentials.scope.includes(role)
 }
 
 module.exports = isInRole
