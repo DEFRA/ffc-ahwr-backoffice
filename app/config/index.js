f278d28c7c63311e93bfe682c8413bed34677e4d
Steve Hunt <st3v3nhunt@users.noreply.github.com>
2022-06-21 16:22:40 +0100

    feat: rename auth enabled env var to match AADAR values

--- app/config/index.js
+++ app/config/index.js
@@ -100,7 +100,7 @@ const config = {
     password: process.env.COOKIE_PASSWORD
   },
   auth: {
-    enabled: process.env.AUTHENTICATION_ENABLED,
+    enabled: process.env.AADAR_ENABLED,
     clientSecret: process.env.AADAR_CLIENT_SECRET,
     clientId: process.env.AADAR_CLIENT_ID,
     authority: `https://login.microsoftonline.com/${process.env.AADAR_TENANT_ID}`,
