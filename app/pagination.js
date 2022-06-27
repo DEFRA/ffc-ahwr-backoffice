580dee0e64168b426036be2b017abaa1e3eba1a3
Satish Chatap <satish.chatap@capgemini.com>
2022-06-27 23:36:27 +0100

    increase page size to 20

--- app/pagination.js
+++ app/pagination.js
@@ -1,9 +1,9 @@
 /**
  * Get limit and offset on basis of page selected for querying data.
  * @param  {} page=1
- * @param  {} limit=10
+ * @param  {} limit=20
  */
-function getPagination (page = 1, limit = 10) {
+function getPagination (page = 1, limit = 20) {
   const offset = page === 1 ? 0 : (page - 1) * limit
   return { limit, offset }
 }
