b0cd36ac408cdcda7443093ae52354f87ce80b02
Shivanshu Patel <shivanshuit914@gmail.com>
2022-07-01 17:05:00 +0100

    feat: remove static content

--- app/routes/view-application.js
+++ app/routes/view-application.js
@@ -3,12 +3,35 @@ const boom = require('@hapi/boom')
 const { getApplication } = require('../messaging/applications')
 const { administrator, processor, user } = require('../auth/permissions')
 
+const head = [
+  {
+    text: "Date"
+  },
+  {
+    text: "Data requested"
+  },
+  {
+    text: "Data entered"
+  }
+]
+
 const getOrganisationRows = (organisation) => {
   return [
     { key: { text: 'SBI number:' }, value: { text: organisation?.sbi }, actions: { items: [{ href: '#', text: 'Change' }] } },
     { key: { text: 'Address:' }, value: { text: organisation?.address }, actions: { items: [{ href: '#', text: 'Change' }] } },
     { key: { text: 'Email address:' }, value: { text: organisation?.email }, actions: { items: [{ href: '#', text: 'Change' }] } }
   ]
+}
+
+const getFarmerApplication = (data) => {
+  return {
+    head,
+    rows: [
+      [{ text: data.createdAt }, { text: "Review type" }, { text: data.whichReview } ],
+      [{ text: data.createdAt }, { text: "Lifestock number" }, { text: data.whichReview } ],
+      [{ text: data.createdAt }, { text: "T&Cs agreed?" }, { text: data.declaration } ]
+    ]
+  }
 }
 
 module.exports = {
@@ -28,7 +51,13 @@ module.exports = {
       }
       const application = response
       console.log(application)
-      return h.view('view-application', { applicationId: application.reference, status: application.status.status, organisationName: application?.data?.organisation?.name, listData: { rows: getOrganisationRows(application?.data?.organisation) } })
+      return h.view('view-application', {
+        applicationId: application.reference,
+        status: application.status.status,
+        organisationName: application?.data?.organisation?.name,
+        application: getFarmerApplication(application?.data),
+        listData: { rows: getOrganisationRows(application?.data?.organisation) }
+      })
     }
   }
 }
