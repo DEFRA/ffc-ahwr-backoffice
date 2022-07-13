7db578cbcaab9f06f16f2e7cb2c2bc89db9dc390
Satish Chatap <satish.chatap@capgemini.com>
2022-07-13 10:41:47 +0100

    fixed helm chart issue & added tests

--- app/api/applications.js
+++ app/api/applications.js
@@ -3,6 +3,9 @@ const { applicationApiUri } = require('../config')
 async function getApplication (reference) {
   const url = `${applicationApiUri}/application/get/${reference}`
   const response = await Wreck.get(url, { json: true })
+  if (response.res.statusCode !== 200) {
+    return null
+  }
   return response.payload
 }
 async function getApplications (searchType, searchText, limit, offset) {
@@ -16,6 +19,9 @@ async function getApplications (searchType, searchText, limit, offset) {
     json: true
   }
   const response = await Wreck.post(url, options)
+  if (response.res.statusCode !== 200) {
+    return { applications: [], total: 0 }
+  }
   return response.payload
 }
 module.exports = {
---
+++ test/unit/api/applications.test.js
@@ -0,0 +1,114 @@
+const Wreck = require('@hapi/wreck')
+jest.mock('@hapi/wreck')
+jest.mock('../../../app/config')
+const { applicationApiUri } = require('../../../app/config')
+const appRef = 'ABC-1234'
+const limit = 20
+const offset = 0
+let searchText = ''
+let searchType = ''
+const { getApplications, getApplication } = require('../../../app/api/applications')
+describe('Application API', () => {
+  it('GetApplications should return empty applications array', async () => {
+    jest.mock('@hapi/wreck')
+    const wreckResponse = {
+      payload: {
+        applications: [],
+        total: 0
+      },
+      res: {
+        statusCode: 502
+      }
+    }
+    const options = {
+      payload: {
+        search: { text: searchText, type: searchType },
+        limit,
+        offset
+      },
+      json: true
+    }
+    Wreck.post = jest.fn(async function (_url, _options) {
+      return wreckResponse
+    })
+    const response = await getApplications(searchType, searchText, limit, offset)
+    expect(response).not.toBeNull()
+    expect(response.applications).toStrictEqual([])
+    expect(response.total).toStrictEqual(0)
+    expect(Wreck.post).toHaveBeenCalledTimes(1)
+    expect(Wreck.post).toHaveBeenCalledWith(`${applicationApiUri}/application/search`, options)
+  })
+  it('GetApplication should return null', async () => {
+    jest.mock('@hapi/wreck')
+    const wreckResponse = {
+      payload: null,
+      res: {
+        statusCode: 502
+      }
+    }
+    const options = { json: true }
+    Wreck.get = jest.fn(async function (_url, _options) {
+      return wreckResponse
+    })
+    const response = await getApplication(appRef)
+    expect(response).toBeNull()
+    expect(Wreck.get).toHaveBeenCalledTimes(1)
+    expect(Wreck.get).toHaveBeenCalledWith(`${applicationApiUri}/application/get/${appRef}`, options)
+  })
+
+  it('GetApplications should return valid applications array', async () => {
+    jest.mock('@hapi/wreck')
+    const wreckResponse = {
+      payload: {
+        applications: [{
+
+        }],
+        total: 1
+      },
+      res: {
+        statusCode: 200
+      }
+    }
+    searchText = '1234567890'
+    searchType = 'sbi'
+    const options = {
+      payload: {
+        search: { text: searchText, type: searchType },
+        limit,
+        offset
+      },
+      json: true
+    }
+    Wreck.post = jest.fn(async function (_url, _options) {
+      return wreckResponse
+    })
+    const response = await getApplications(searchType, searchText, limit, offset)
+    expect(response).not.toBeNull()
+    expect(response.applications).toStrictEqual([{}])
+    expect(response.total).toStrictEqual(1)
+    expect(Wreck.post).toHaveBeenCalledTimes(1)
+    expect(Wreck.post).toHaveBeenCalledWith(`${applicationApiUri}/application/search`, options)
+  })
+  it('GetApplication should not return null', async () => {
+    jest.mock('@hapi/wreck')
+    const applicationData = {
+      reference: appRef
+    }
+    const wreckResponse = {
+      payload: applicationData,
+      res: {
+        statusCode: 200
+      }
+    }
+    const options = { json: true }
+    Wreck.get = jest.fn(async function (_url, _options) {
+      return wreckResponse
+    })
+    const response = await getApplication(appRef)
+    expect(response).not.toBeNull()
+    expect(response).toBe(applicationData)
+    expect(response.reference).toBe(appRef)
+    expect(Wreck.get).toHaveBeenCalledTimes(1)
+    expect(Wreck.get).toHaveBeenCalledWith(`${applicationApiUri}/application/get/${appRef}`, options)
+  })
+})
