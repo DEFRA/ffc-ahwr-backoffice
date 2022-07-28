1d86bea37060fd3edd08b31231a2087d9b138196
Satish Chatap <satish.chatap@capgemini.com>
2022-07-28 09:56:30 +0100

    more tests

--- app/routes/home.js
+++ app/routes/home.js
@@ -1,7 +1,9 @@
+const { administrator, processor, user } = require('../auth/permissions')
 module.exports = {
   method: 'GET',
   path: '/',
   options: {
+    auth: { scope: [administrator, processor, user] },
     handler: async (_, h) => {
       return h.view('home')
     }
---
+++ test/integration/narrow/routes/applications.filter.test.js
@@ -0,0 +1,55 @@
+const cheerio = require('cheerio')
+const expectPhaseBanner = require('../../../utils/phase-banner-expect')
+const { administrator } = require('../../../../app/auth/permissions')
+const sessionMock = require('../../../../app/session')
+const applicationData = require('.././../../data/applications.json')
+
+jest.mock('../../../../app/session')
+const applications = require('../../../../app/api/applications')
+jest.mock('../../../../app/api/applications')
+const pagination = require('../../../../app/pagination')
+jest.mock('../../../../app/pagination')
+
+pagination.getPagination = jest.fn().mockReturnValue({
+  limit: 10, offset: 0
+})
+
+pagination.getPagingData = jest.fn().mockReturnValue({
+  page: 1, totalPages: 1, total: 1, limit: 10, url: undefined
+})
+applications.getApplications = jest.fn().mockReturnValue(applicationData)
+sessionMock.getAppSearch = jest.fn().mockReturnValue([])
+describe('Applications Filter test', () => {
+  const url = '/applications/remove'
+  jest.mock('../../../../app/auth')
+  const auth = { strategy: 'session-auth', credentials: { scope: [administrator] } }
+  const method = 'GET'
+  beforeEach(() => {
+    jest.clearAllMocks()
+  })
+  describe(`GET ${url} route`, () => {
+    test('returns 302 no auth', async () => {
+      const options = {
+        method,
+        url: `${url}/PENDING`
+      }
+      const res = await global.__SERVER__.inject(options)
+      expect(res.statusCode).toBe(302)
+    })
+    test('returns 200', async () => {
+      const options = {
+        method,
+        url: `${url}/PENDING`,
+        auth
+      }
+      const res = await global.__SERVER__.inject(options)
+      expect(res.statusCode).toBe(200)
+      const $ = cheerio.load(res.payload)
+      expect($('h1.govuk-heading-l').text()).toEqual('Applications')
+      expect($('title').text()).toContain('Applications')
+      expect(sessionMock.getAppSearch).toHaveBeenCalledTimes(4)
+      expect(sessionMock.setAppSearch).toHaveBeenCalledTimes(1)
+      expectPhaseBanner.ok($)
+    })
+  })
+})
