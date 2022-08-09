8178ea22ea35da7d1d8814a09793ab6cf296df12
Julian Taylor <julian.taylor@capgemini.com>
2022-08-09 10:58:00 +0100

    WIP. Add in toggle functionality.

--- app/frontend/src/entry.js
+++ app/frontend/src/entry.js
@@ -6,3 +6,21 @@ import moj from '@ministryofjustice/frontend'
 initAll()
 window.$ = $
 moj.initAll()
+
+new moj.FilterToggleButton({
+  bigModeMediaQuery: '(min-width: 48.063em)',
+  startHidden: true,
+  toggleButton: {
+    container: $('.moj-action-bar__filter'),
+    showText: 'Show filter',
+    hideText: 'Hide filter',
+    classes: 'govuk-button--secondary'
+  },
+  closeButton: {
+    container: $('.moj-filter__header-action'),
+    text: 'Close'
+  },
+  filter: {
+    container: $('.moj-filter')
+  }
+})
