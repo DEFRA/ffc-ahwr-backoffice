d00529e048edf7667441481520ebebdfacdbf767
Shivanshu Patel <shivanshuit914@gmail.com>
2022-08-10 09:52:09 +0100

    feat: extract out auth config to separate file

---
+++ app/config/auth.js
@@ -0,0 +1,26 @@
+
+const Joi = require('joi')
+
+const schema = Joi.object({
+  enabled: Joi.boolean().default(false),
+  clientSecret: Joi.string().allow(''),
+  clientId: Joi.string().allow(''),
+  authority: Joi.string().allow(''),
+  redirectUrl: Joi.string().default('http://localhost:3002/authenticate')
+})
+
+const config = {
+  enabled: process.env.AADAR_ENABLED,
+  clientSecret: process.env.AADAR_CLIENT_SECRET,
+  clientId: process.env.AADAR_CLIENT_ID,
+  authority: `https://login.microsoftonline.com/${process.env.AADAR_TENANT_ID}`,
+  redirectUrl: process.env.AADAR_REDIRECT_URL
+}
+
+const { error, value } = schema.validate(config, { abortEarly: false })
+
+if (error) {
+  throw new Error(`The auth config is invalid. ${error.message}`)
+}
+
+module.exports = value
