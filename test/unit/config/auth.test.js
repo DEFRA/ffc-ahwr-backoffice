bf73fee0113160b3f88c87d35131170bd3fa47ea
Shivanshu Patel <shivanshuit914@gmail.com>
2022-08-10 12:53:58 +0100

    feat: extract out auth config to separate file (#30)
    * feat: extract out auth config to separate file
    
    * bump: version
    
    * fix: linting
    
    * feat: add tests

---
+++ test/unit/config/auth.test.js
@@ -0,0 +1,27 @@
+const authConfig = require('../../../app/config/auth')
+
+describe('cache Config Test', () => {
+  const OLD_ENV = process.env
+
+  beforeEach(() => {
+    jest.resetModules()
+    process.env = { ...OLD_ENV }
+  })
+
+  afterAll(() => {
+    process.env = OLD_ENV
+  })
+  test('Should pass validation for all fields populated', async () => {
+    expect(authConfig).toBeDefined()
+  })
+
+  test('Invalid env var throws error', () => {
+    try {
+      process.env.AADAR_CLIENT_SECRET = null
+      process.env.AADAR_CLIENT_ID = null
+      require('../../../app/config/auth')
+    } catch (err) {
+      expect(err.message).toBe('The auth config is invalid. "clientSecret" must be a string. "clientId" must be a string')
+    }
+  })
+})
