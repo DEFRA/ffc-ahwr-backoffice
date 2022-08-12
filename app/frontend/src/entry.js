8d4d9139f3b0b91a8f59ab76c232cbe63f8fca1d
Satish Chatap <satish.chatap@capgemini.com>
2022-08-12 09:36:17 +0100

    false sonar code smell

--- app/frontend/src/entry.js
+++ app/frontend/src/entry.js
@@ -7,20 +7,20 @@ initAll()
 window.$ = $
 moj.initAll()
 /* eslint-disable no-new */
-new moj.FilterToggleButton({
-    bigModeMediaQuery: "(min-width: 48.063em)",
-    startHidden: true,
-    toggleButton: {
-      container: $(".moj-action-bar__filter"),
-      showText: "Show filter",
-      hideText: "Hide filter",
-      classes: "govuk-button--secondary",
-    },
-    closeButton: {
-      container: $(".moj-filter__header-action"),
-      text: "Close",
-    },
-    filter: {
-      container: $(".moj-filter"),
-    },
-  });
+new moj.FilterToggleButton({ // NOSONAR
+  bigModeMediaQuery: '(min-width: 48.063em)',
+  startHidden: true,
+  toggleButton: {
+    container: $('.moj-action-bar__filter'),
+    showText: 'Show filter',
+    hideText: 'Hide filter',
+    classes: 'govuk-button--secondary'
+  },
+  closeButton: {
+    container: $('.moj-filter__header-action'),
+    text: 'Close'
+  },
+  filter: {
+    container: $('.moj-filter')
+  }
+})
