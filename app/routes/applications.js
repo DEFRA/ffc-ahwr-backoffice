3fd0f8a35360e55884786466c8b9a10e5f05c1ab
Shivanshu Patel <shivanshuit914@gmail.com>
2022-08-17 09:10:26 +0100

    feat: refactor applications route

--- app/routes/applications.js
+++ app/routes/applications.js
@@ -1,91 +1,12 @@
 const viewTemplate = 'applications'
 const currentPath = `/${viewTemplate}`
-const { getApplications } = require('../api/applications')
-const { getPagination, getPagingData, displayPageSize } = require('../pagination')
+const { displayPageSize } = require('../pagination')
 const Joi = require('joi')
 const { setAppSearch, getAppSearch } = require('../session')
 const keys = require('../session/keys')
-const getStyleClassByStatus = require('../constants/status')
 const { administrator, processor, user } = require('../auth/permissions')
-async function createModel (request, page) {
-  page = page ?? request.query.page ?? 1
-  const { limit, offset } = getPagination(page)
-  const path = request.headers.path ?? ''
-  const searchText = getAppSearch(request, keys.appSearch.searchText)
-  const searchType = getAppSearch(request, keys.appSearch.searchType)
-  const filterStatus = getAppSearch(request, keys.appSearch.filterStatus) ?? []
-  const apps = await getApplications(searchType, searchText, limit, offset, filterStatus)
-  if (apps.total > 0) {
-    let statusClass
-    const applications = apps.applications.map(n => {
-      statusClass = getStyleClassByStatus(n.status.status)
-      return [
-        { text: n.reference },
-        { text: n.data?.organisation?.name },
-        {
-          text: n.data?.organisation?.sbi,
-          format: 'numeric',
-          attributes: {
-            'data-sort-value': n.data?.organisation?.sbi
-          }
-        },
-        {
-          text: new Date(n.createdAt).toLocaleDateString('en-GB'),
-          format: 'date',
-          attributes: {
-            'data-sort-value': n.createdAt
-          }
-        },
-        {
-          html: `<span class="govuk-tag ${statusClass}">${n.status.status}</span>`,
-          attributes: {
-            'data-sort-value': `${n.status.status}`
-          }
-        },
-        { html: `<a href="view-application/${n.reference}">View application</a>` }
-      ]
-    })
-    const pagingData = getPagingData(apps.total ?? 0, limit, page, path)
-    const groupByStatus = apps.applicationStatus.map(s => {
-      return {
-        status: s.status,
-        total: s.total,
-        styleClass: getStyleClassByStatus(s.status),
-        selected: filterStatus.filter(f => f === s.status).length > 0
-      }
-    })
-    return {
-      applications,
-      ...pagingData,
-      searchText,
-      availableStatus: groupByStatus,
-      selectedStatus: groupByStatus.filter(s => s.selected === true).map(s => {
-        return {
-          href: `${currentPath}/remove/${s.status}`,
-          classes: s.styleClass,
-          text: s.status
-        }
-      }),
-      filterStatus: groupByStatus.map(s => {
-        return {
-          value: s.status,
-          html: `<div class="govuk-tag ${s.styleClass}"  style="color:#104189;" >${s.status} (${s.total}) </div>`,
-          checked: s.selected,
-          styleClass: s.styleClass
-        }
-      })
-    }
-  } else {
-    return {
-      applications: [],
-      error: 'No Applications found.',
-      searchText,
-      availableStatus: [],
-      selectedStatus: [],
-      filterStatus: []
-    }
-  }
-}
+const ViewModel = require('./models/application-list')
+
 const appRefRegEx = /^vv-[\da-f]{4}-[\da-f]{4}$/i
 const validStatus = ['applied', 'withdrawn', 'data inputted', 'claimed', 'check', 'accepted', 'rejected', 'paid']
 const sbiRegEx = /^[\0-9]{9}$/i
@@ -129,7 +50,7 @@ module.exports = [
         })
       },
       handler: async (request, h) => {
-        return h.view(viewTemplate, await createModel(request))
+        return h.view(viewTemplate, await new ViewModel(request))
       }
     }
   },
@@ -140,7 +61,7 @@ module.exports = [
       auth: { scope: [administrator, processor, user] },
       handler: async (request, h) => {
         setAppSearch(request, keys.appSearch.filterStatus, [])
-        return h.view(viewTemplate, await createModel(request))
+        return h.view(viewTemplate, await new ViewModel(request))
       }
     }
   },
@@ -158,7 +79,7 @@ module.exports = [
         let filterStatus = getAppSearch(request, keys.appSearch.filterStatus)
         filterStatus = filterStatus.filter(s => s !== request.params.status)
         setAppSearch(request, keys.appSearch.filterStatus, filterStatus)
-        return h.view(viewTemplate, await createModel(request))
+        return h.view(viewTemplate, await new ViewModel(request))
       }
     }
   },
@@ -186,7 +107,7 @@ module.exports = [
           const { searchText, searchType } = checkValidSearch(request.payload.searchText)
           setAppSearch(request, keys.appSearch.searchText, searchText ?? '')
           setAppSearch(request, keys.appSearch.searchType, searchType ?? '')
-          return h.view(viewTemplate, await createModel(request, 1))
+          return h.view(viewTemplate, await new ViewModel(request, 1))
         } catch (err) {
           return h.view(viewTemplate, { ...request.payload, error: err }).code(400).takeover()
         }
