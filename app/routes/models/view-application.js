f5d068e26753b83fb109d0c7c5485e80ce9f8e31
Shivanshu Patel <shivanshuit914@gmail.com>
2022-10-14 10:09:10 +0100

    remove: vet tab (#40)
    * remove: vet tab
    
    * bump: version
    
    * fix: linting, remove vet tabs unit tests

--- app/routes/models/view-application.js
+++ app/routes/models/view-application.js
@@ -1,5 +1,4 @@
 const getFarmerApplication = require('./farmer-application')
-const getVetVisitData = require('./vet-visit-review')
 const getOrganisationRows = require('./application-organisation')
 const getPaymentData = require('./application-payment')
 const getClaimData = require('./application-claim')
@@ -7,7 +6,6 @@ const getClaimData = require('./application-claim')
 function ViewModel (application) {
   this.model = {
     applicationData: getFarmerApplication(application),
-    vetVisitData: application?.vetVisit ? getVetVisitData(application.vetVisit, application?.data?.whichReview) : false,
     listData: { rows: getOrganisationRows(application?.data?.organisation) },
     paymentData: application?.payment ? getPaymentData(application?.payment) : false,
     claimData: application?.claimed ? getClaimData(application?.updatedAt) : false
