e77009958e6be55cbde9679f6165e9ba82e391f2
Shivanshu Patel <shivanshuit914@gmail.com>
2022-11-08 10:12:12 +0000

    feat: add claim tab data

--- app/routes/models/application-claim.js
+++ app/routes/models/application-claim.js
@@ -1,11 +1,16 @@
 const { formatedDateToUk } = require('../../lib/display-helper')
 
-module.exports = (updatedAt) => {
-  const formatedDate = formatedDateToUk(updatedAt)
+module.exports = (application) => {
+  const { data } = application
+  const formatedDate = formatedDateToUk(application.updatedAt)
   return {
     head: [{ text: 'Date' }, { text: 'Data requested' }, { text: 'Data entered' }],
     rows: [
-      [{ text: formatedDate }, { text: 'Details correct?' }, { text: 'Yes' }]
+      [{ text: formatedDate }, { text: 'Details correct?' }, { text: data?.confirmCheckDetails }],
+      [{ text: formatedDate }, { text: 'Date of review' }, { text: formatedDateToUk(data?.visitDate) }],
+      [{ text: formatedDate }, { text: 'Vet’s name' }, { text: data?.vetName }],
+      [{ text: formatedDate }, { text: 'Vet’s RCVS number' }, { text: data?.vetRcvs }],
+      [{ text: formatedDate }, { text: 'Unique reference number (URN)' }, { text: data?.urnResult }]
     ]
   }
 }
