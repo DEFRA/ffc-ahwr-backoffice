153dd07f1d46f4b8e69972b186cabaf63e24d867
Shivanshu Patel <shivanshuit914@gmail.com>
2022-12-07 19:51:41 +0000

    feat: add in check tab data

--- app/routes/models/view-application.js
+++ app/routes/models/view-application.js
@@ -2,11 +2,13 @@ const getFarmerApplication = require('./farmer-application')
 const getOrganisationRows = require('./application-organisation')
 const getClaimData = require('./application-claim')
 
+const claimDataStatus = ['IN CHECK', 'REJECTED', 'READY TO PAY']
+
 function ViewModel (application) {
   this.model = {
     applicationData: getFarmerApplication(application),
     listData: { rows: getOrganisationRows(application?.data?.organisation) },
-    claimData: application?.claimed ? getClaimData(application) : false
+    claimData: application?.claimed || claimDataStatus.includes(application?.status?.status) ? getClaimData(application) : false
   }
 }
 
