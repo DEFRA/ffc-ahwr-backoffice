87a6111e7b946993e0c109240abb8441b0d6cbd1
chrissmKainos <christophersm@kainos.com>
2023-02-28 11:30:43 +0000

    159410: Additional test added

--- test/integration/narrow/routes/models/application-history.test.js
+++ test/integration/narrow/routes/models/application-history.test.js
@@ -32,4 +32,38 @@ describe('Application-history model test', () => {
     expect(res.header[3].text).toEqual('User')
     expect(res.rows).toBe(undefined)
   })
+  test.each([
+    { statusId: 1, expectedStatus: 'Agreement agreed' },
+    { statusId: 2, expectedStatus: 'Withdraw completed' },
+    { statusId: 3, expectedStatus: 'Data inputted' },
+    { statusId: 4, expectedStatus: 'Claimed' },
+    { statusId: 5, expectedStatus: 'Claim in check' },
+    { statusId: 6, expectedStatus: 'Agreement accepted' },
+    { statusId: 7, expectedStatus: 'Agreement not agreed' },
+    { statusId: 8, expectedStatus: 'Claim paid' },
+    { statusId: 9, expectedStatus: 'Claim approved' },
+    { statusId: 10, expectedStatus: 'Claim rejected' }
+  ])('getHistoryData - Valid status field', async ({ statusId, expectedStatus }) => {
+    const applicationHistoryData = {
+      historyRecords: [
+        {
+          date: '23/03/2023',
+          time: '10:00:12',
+          statusId: statusId,
+          user: 'Daniel Jones'
+        }
+      ]
+    }
+    const res = getHistoryData(applicationHistoryData)
+    expect(res).not.toBeNull()
+    expect(res.header[0].text).toEqual('Date')
+    expect(res.header[1].text).toEqual('Time')
+    expect(res.header[2].text).toEqual('Action')
+    expect(res.header[3].text).toEqual('User')
+    expect(res.rows.length).toEqual(1)
+    expect(res.rows[0][0].text).toEqual('23/03/2023')
+    expect(res.rows[0][1].text).toEqual('10:00:12')
+    expect(res.rows[0][2].text).toEqual(expectedStatus)
+    expect(res.rows[0][3].text).toEqual('Daniel Jones')
+  })
 })
