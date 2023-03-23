c0d0276a31ed88e6186186a05b9158aca0f7a45c
json-hamilton <jason.hamilton@kainos.com>
2023-03-23 15:06:56 +0000

    Fixed import

--- app/frontend/src/entry.js
+++ app/frontend/src/entry.js
@@ -1,7 +1,7 @@
 import { initAll } from 'govuk-frontend'
 import './css/application.scss'
 import './js/cookies'
-import '.js/handleDuplicateFormSubmissions'
+import './js/handleDuplicateFormSubmissions'
 import $ from 'jquery'
 import moj from '@ministryofjustice/frontend'
 initAll()
