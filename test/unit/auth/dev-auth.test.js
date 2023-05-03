c530cb349910353d7ce006ba81ef718246659a2b
chrissmKainos <christophersm@kainos.com>
2023-05-03 10:42:58 +0100

    180424: Update to unit tests

--- test/unit/auth/dev-auth.test.js
+++ test/unit/auth/dev-auth.test.js
@@ -1,14 +1,22 @@
 const devAuth = require('../../../app/auth/dev-auth')
+const { administrator, processor, user, recommender, authoriser } = require('../../../app/auth/permissions')
+const MOCK_COOKIE_AUTH_SET = jest.fn()
 
-describe('Deve auth test', () => {
+describe('Dev auth test', () => {
+  afterEach(() => {
+    jest.clearAllMocks()
+  })
+
   test('getAuthenticationUrl test', () => {
     expect(devAuth.getAuthenticationUrl()).toBe('/dev-auth')
   })
-  test('authenticate test', () => {
-    expect(devAuth.authenticate).toBeDefined()
+  test('authenticate test', async () => {
+    await devAuth.authenticate(expect.anything(), { set: MOCK_COOKIE_AUTH_SET })
+    expect(MOCK_COOKIE_AUTH_SET).toHaveBeenCalledTimes(1)
   })
-  test('refresh test', () => {
-    expect(devAuth.refresh).toBeDefined()
+  test('refresh test', async () => {
+    expect(await devAuth.refresh(expect.anything(), { set: MOCK_COOKIE_AUTH_SET }, false)).toEqual([administrator, processor, user, recommender, authoriser])
+    expect(MOCK_COOKIE_AUTH_SET).toHaveBeenCalledTimes(1)
   })
   test('logout test', () => {
     expect(devAuth.logout).toBeDefined()
