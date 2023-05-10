f7bc646e06a7ea2e1a62fa80f8206afe20749a22
json-hamilton <jason.hamilton@kainos.com>
2023-05-10 12:25:18 +0100

    Added tests

--- test/unit/api/stage-configuration.test.js
+++ test/unit/api/stage-configuration.test.js
@@ -3,26 +3,73 @@ jest.mock('@hapi/wreck')
 jest.mock('../../../app/config')
 const { applicationApiUri } = require('../../../app/config')
 const { getAllStageConfigurations } = require('../../../app/api/stage-configuration')
+
 describe('Stage Configuration API', () => {
-  test('getAllStageConfigurations should return valid stage configuration array', async () => {
-    const wreckResponse = {
-      payload: {
-        stageConfigurations: ['stage1', 'stage2']
-      },
-      res: {
-        statusCode: 200
+  let logSpy
+
+  beforeEach(() => {
+    logSpy = jest.spyOn(console, 'log')
+  })
+
+  afterEach(() => {
+    logSpy.mockRestore()
+  })
+
+  describe('getAllStageConfigurations', () => {
+    test('getAllStageConfigurations should return valid stage configuration array', async () => {
+      const wreckResponse = {
+        payload: {
+          stageConfigurations: ['stage1', 'stage2']
+        },
+        res: {
+          statusCode: 200
+        }
       }
-    }
-    const options = {
-      json: true
-    }
-    Wreck.get = jest.fn(async function (_url, _options) {
-      return wreckResponse
+      const options = {
+        json: true
+      }
+      Wreck.get = jest.fn(async function (_url, _options) {
+        return wreckResponse
+      })
+      const response = await getAllStageConfigurations()
+      expect(response).not.toBeNull()
+      expect(response.stageConfigurations).toStrictEqual(['stage1', 'stage2'])
+      expect(Wreck.get).toHaveBeenCalledTimes(1)
+      expect(Wreck.get).toHaveBeenCalledWith(`${applicationApiUri}/stageconfiguration`, options)
     })
-    const response = await getAllStageConfigurations()
-    expect(response).not.toBeNull()
-    expect(response.stageConfigurations).toStrictEqual(['stage1', 'stage2'])
-    expect(Wreck.get).toHaveBeenCalledTimes(1)
-    expect(Wreck.get).toHaveBeenCalledWith(`${applicationApiUri}/stageconfiguration`, options)
+    test('getAllstageConfigurations should return null', async () => {
+      const wreckResponse = {
+        payload: {
+          stageConfigurations: ['stage1', 'stage2']
+        },
+        res: {
+          statusCode: 404
+        }
+      }
+      const options = {
+        json: true
+      }
+      Wreck.get = jest.fn(async function (_url, _options) {
+        return wreckResponse
+      })
+      const response = await getAllStageConfigurations()
+      expect(response).toBeNull()
+      expect(Wreck.get).toHaveBeenCalledTimes(1)
+      expect(Wreck.get).toHaveBeenCalledWith(`${applicationApiUri}/stageconfiguration`, options)
+    })
+    test('getAllstageConfigurations should return null when error thrown', async () => {
+      const options = {
+        json: true
+      }
+      Wreck.get = jest.fn(async function (_url, _options) {
+        throw new Error('Error')
+      })
+      const response = await getAllStageConfigurations()
+      expect(response).toBeNull()
+      expect(logSpy).toHaveBeenCalledTimes(1)
+      expect(logSpy).toHaveBeenCalledWith(new Error('Error'))
+      expect(Wreck.get).toHaveBeenCalledTimes(1)
+      expect(Wreck.get).toHaveBeenCalledWith(`${applicationApiUri}/stageconfiguration`, options)
+    })
   })
 })
