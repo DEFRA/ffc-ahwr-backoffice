9ddcb9cc48ff22ed3329df3e2f8332b3757b74af
json-hamilton <jason.hamilton@kainos.com>
2023-05-17 09:20:22 +0100

    Added tests

--- app/routes/utils/process-stage-actions.js
+++ app/routes/utils/process-stage-actions.js
@@ -5,40 +5,48 @@ const getUser = require('../../auth/get-user')
 const applicationStageActions = require('../../constants/application-stage-execution-actions')
 
 const processStageActions = async (request, role, stage, action, isClaimToBePaid) => {
-  const userName = getUser(request).username
-  const stageConfigurations = await getAllStageConfigurations()
-  const step = stageConfigurations
-    .find(configuration => configuration.role.roles.includes(role) &&
+  try {
+    const userName = getUser(request).username
+    const stageConfigurations = await getAllStageConfigurations()
+    const step = stageConfigurations
+      .find(configuration => configuration.role.roles.includes(role) &&
                 configuration.stage === stage)
-
-  const results = []
-  let stageExecutionRow
-  const stepId = step.id
-  const payload = {
-    applicationReference: request.payload.reference,
-    stageConfigurationId: stepId,
-    executedAt: new Date(),
-    executedBy: userName,
-    action: {
-      action
+    if (!step) {
+      throw new Error(`Error when filtering stage configurations for role ${role} and stage ${stage}`)
     }
-  }
-
-  for (const stageAction of step.action.actions) {
-    switch (stageAction) {
-      case applicationStageActions.addStageExecutionEntry:
-        stageExecutionRow = await addStageExecution(payload)
-        results.push({ action: 'Added stage execution', stageExecutionRow })
-        break
-      case applicationStageActions.processApplicationClaim:
-        results.push({ action: 'Processed claim', response: await processApplicationClaim(request.payload.reference, userName, isClaimToBePaid) })
-        break
-      case applicationStageActions.updateStageExecutionEntry:
-        results.push({ action: 'Updated stage execution', response: await updateStageExecution(stageExecutionRow.id) })
-        break
+    const results = []
+    let stageExecutionRow
+    const stepId = step.id
+    const payload = {
+      applicationReference: request.payload.reference,
+      stageConfigurationId: stepId,
+      executedAt: new Date(),
+      executedBy: userName,
+      action: {
+        action
+      }
     }
+
+    for (const stageAction of step.action.actions) {
+      switch (stageAction) {
+        case applicationStageActions.addStageExecutionEntry:
+          stageExecutionRow = await addStageExecution(payload)
+          results.push({ action: 'Added stage execution', stageExecutionRow })
+          break
+        case applicationStageActions.processApplicationClaim:
+          results.push({ action: 'Processed claim', response: await processApplicationClaim(request.payload.reference, userName, isClaimToBePaid) })
+          break
+        case applicationStageActions.updateStageExecutionEntry:
+          results.push({ action: 'Updated stage execution', response: await updateStageExecution(stageExecutionRow.id) })
+          break
+      }
+    }
+    return results
+  } catch (error) {
+    console.log('Backoffice: processStageActions error: ', error.message)
+    console.error(error)
+    return []
   }
-  return results
 }
 
 module.exports = processStageActions
