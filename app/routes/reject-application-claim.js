ccd359c4b652052294db6ac32fd164a67108a78f
Marcin Mogieła <118533775+marcin-mogiela@users.noreply.github.com>
2023-05-18 15:13:07 +0200

    FFC-175698: add error handling to authorise reject claim confirmation form; (#105)

--- app/routes/reject-application-claim.js
+++ app/routes/reject-application-claim.js
@@ -1,4 +1,5 @@
 const Joi = require('joi')
+const config = require('../config')
 const { processApplicationClaim } = require('../api/applications')
 const getUser = require('../auth/get-user')
 const preDoubleSubmitHandler = require('./utils/pre-submission-handler')
@@ -10,19 +11,50 @@ module.exports = {
   options: {
     pre: [{ method: preDoubleSubmitHandler }],
     validate: {
-      payload: Joi.object({
-        rejectClaim: Joi.string().valid('yes', 'no'),
-        reference: Joi.string().valid(),
-        page: Joi.number().greater(0).default(1)
-      })
+      payload: Joi.object(config.rbac.enabled
+        ? {
+            confirm: Joi.array().items(Joi.string().valid('rejectClaim', 'sentChecklist')).required(),
+            reference: Joi.string().valid(),
+            page: Joi.number().greater(0).default(1)
+          }
+        : {
+            rejectClaim: Joi.string().valid('yes', 'no'),
+            reference: Joi.string().valid(),
+            page: Joi.number().greater(0).default(1)
+          }),
+      failAction: async (request, h, error) => {
+        console.log(`routes:reject-application-claim: Error when validating payload: ${JSON.stringify({
+          errorMessage: error.message,
+          payload: request.payload
+        })}`)
+        const errors = []
+        if (error.details && error.details[0].context.key === 'confirm') {
+          errors.push({
+            text: 'You must select both checkboxes',
+            href: '#reject-claim-panel'
+          })
+        }
+        return h
+          .redirect(`/view-application/${request.payload.reference}?page=${request?.payload?.page || 1}&errors=${encodeURIComponent(JSON.stringify(errors))}`)
+          .takeover()
+      }
     },
     handler: async (request, h) => {
-      if (request.payload.rejectClaim === 'yes') {
-        const userName = getUser(request).username
-        await processApplicationClaim(request.payload.reference, userName, false)
-        await crumbCache.generateNewCrumb(request, h)
+      if (config.rbac.enabled) {
+        if (JSON.stringify(request.payload.confirm) === JSON.stringify(['rejectClaim', 'sentChecklist'])) {
+          const userName = getUser(request).username
+          await processApplicationClaim(request.payload.reference, userName, false)
+          await crumbCache.generateNewCrumb(request, h)
+        }
+        return h.redirect(`/view-application/${request.payload.reference}?page=${request?.payload?.page || 1}`)
+      } else {
+        if (request.payload.rejectClaim === 'yes') {
+          const userName = getUser(request).username
+          await processApplicationClaim(request.payload.reference, userName, false)
+          await crumbCache.generateNewCrumb(request, h)
+        }
+        return h.redirect(`/view-application/${request.payload.reference}?page=${request?.payload?.page || 1}`)
       }
-      return h.redirect(`/view-application/${request.payload.reference}?page=${request?.payload?.page || 1}`)
     }
   }
 }
