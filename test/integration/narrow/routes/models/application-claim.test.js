37659938d6f1f29f4acd107b48c0000f07574040
chrissmKainos <christophersm@kainos.com>
2023-07-03 13:56:22 +0100

    203182: Additional test added

--- test/integration/narrow/routes/models/application-claim.test.js
+++ test/integration/narrow/routes/models/application-claim.test.js
@@ -56,4 +56,29 @@ describe('Application-claim model test', () => {
     expect(res.rows[5][0].text).toEqual('Test results unique reference number (URN)')
     expect(res.rows[5][1].text).toEqual('134242')
   })
+
+  test('getClaimData - Valid Data with no claim-claimed event', async () => {
+    const res = getClaimData(viewApplicationData.claimWithNoClaimDate, {
+      "eventRecords": [
+        {
+          "EventRaised": "2022-11-09T11:00:00.000Z",
+          "EventType": "claim-createdBy"
+        }
+      ]
+    })
+    expect(res).not.toBeNull()
+    expect(res.rows.length).toEqual(6)
+    expect(res.rows[0][0].text).toEqual('Date of claim')
+    expect(res.rows[0][1].text).toEqual('')
+    expect(res.rows[1][0].text).toEqual('Review details confirmed')
+    expect(res.rows[1][1].text).toEqual('Yes')
+    expect(res.rows[2][0].text).toEqual('Date of review')
+    expect(res.rows[2][1].text).toEqual('07/11/2022')
+    expect(res.rows[3][0].text).toEqual('Vet’s name')
+    expect(res.rows[3][1].text).toEqual('testVet')
+    expect(res.rows[4][0].text).toEqual('Vet’s RCVS number')
+    expect(res.rows[4][1].text).toEqual('1234234')
+    expect(res.rows[5][0].text).toEqual('Test results unique reference number (URN)')
+    expect(res.rows[5][1].text).toEqual('134242')
+  })
 })
