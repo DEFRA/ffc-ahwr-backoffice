5ffdf1c132a1371db3e25a2e7ca44f82480bdb56
chrissmKainos <christophersm@kainos.com>
2023-07-07 13:15:54 +0100

    203183: New unit test added

---
+++ test/unit/app/routes/utils/parse-data.test.js
@@ -0,0 +1,42 @@
+
+const { parseData } = require('../../../../../app/routes/utils/parse-data')
+const applicationEventData = require('../../../../data/application-events.json')
+
+describe('Parse data tests', () => { 
+  let logSpy
+  beforeAll(() => {
+    jest.clearAllMocks()
+    logSpy = jest.spyOn(console, 'log')
+  })
+  afterEach(() => {
+    jest.clearAllMocks()
+  })
+
+  test('Parse data - Valid payload', async () => {
+    const parsedData = parseData(applicationEventData.eventRecords, 'claim-claimed', 'claimed')
+
+    expect(parsedData.value).toBeFalsy
+    expect(parsedData.raisedOn).toBe('2022-11-09T11:36:00.000Z')
+    expect(parsedData.raisedBy).toBe('testuser@test.com')
+  })
+
+  test('Parse data - Empty payload', async () => {
+    const parsedData = parseData(
+      [
+            {
+                "EventRaised": "2022-11-09T11:00:00.000Z",
+                "EventType": "claim-createdBy"
+            },
+            {               
+              "EventRaised": "2022-11-01T11:00:01.000Z",
+              "EventType": "claim-claimed",
+              "Payload": "{}"
+            }
+        ]
+  , 'claim-claimed', 'claimed')
+
+    expect(parsedData.value).toBe('')
+    expect(parsedData.raisedOn).toBe('')
+    expect(parsedData.raisedBy).toBe('')
+  })
+})
