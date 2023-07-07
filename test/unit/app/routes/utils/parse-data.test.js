95ee500696990dd56111ae13154edd47c9c20ac4
chrissmKainos <christophersm@kainos.com>
2023-07-07 13:24:18 +0100

    203183: Fixed unit test and linting issues

--- test/unit/app/routes/utils/parse-data.test.js
+++ test/unit/app/routes/utils/parse-data.test.js
@@ -2,20 +2,11 @@
 const { parseData } = require('../../../../../app/routes/utils/parse-data')
 const applicationEventData = require('../../../../data/application-events.json')
 
-describe('Parse data tests', () => { 
-  let logSpy
-  beforeAll(() => {
-    jest.clearAllMocks()
-    logSpy = jest.spyOn(console, 'log')
-  })
-  afterEach(() => {
-    jest.clearAllMocks()
-  })
-
+describe('Parse data tests', () => {
   test('Parse data - Valid payload', async () => {
     const parsedData = parseData(applicationEventData.eventRecords, 'claim-claimed', 'claimed')
 
-    expect(parsedData.value).toBeFalsy
+    expect(parsedData.value).toBeFalsy()
     expect(parsedData.raisedOn).toBe('2022-11-09T11:36:00.000Z')
     expect(parsedData.raisedBy).toBe('testuser@test.com')
   })
@@ -23,17 +14,17 @@ describe('Parse data tests', () => {
   test('Parse data - Empty payload', async () => {
     const parsedData = parseData(
       [
-            {
-                "EventRaised": "2022-11-09T11:00:00.000Z",
-                "EventType": "claim-createdBy"
-            },
-            {               
-              "EventRaised": "2022-11-01T11:00:01.000Z",
-              "EventType": "claim-claimed",
-              "Payload": "{}"
-            }
-        ]
-  , 'claim-claimed', 'claimed')
+        {
+          EventRaised: '2022-11-09T11:00:00.000Z',
+          EventType: 'claim-createdBy'
+        },
+        {
+          EventRaised: '2022-11-01T11:00:01.000Z',
+          EventType: 'claim-claimed',
+          Payload: '{}'
+        }
+      ]
+      , 'claim-claimed', 'claimed')
 
     expect(parsedData.value).toBe('')
     expect(parsedData.raisedOn).toBe('')
