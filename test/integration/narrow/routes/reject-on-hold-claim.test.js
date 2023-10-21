f03c9e6f695c97bb659dafc6698d8ca248ea0fe7
govTechSatish <satish.chatap@capgemini.com>
2023-10-21 13:11:50 +0100

    added more test

---
+++ test/integration/narrow/routes/reject-on-hold-claim.test.js
@@ -0,0 +1,164 @@
+const cheerio = require('cheerio')
+const expectPhaseBanner = require('../../../utils/phase-banner-expect')
+const { administrator, authoriser } = require('../../../../app/auth/permissions')
+const getCrumbs = require('../../../utils/get-crumbs')
+
+const reference = 'AHWR-555A-FD4C'
+const encodedEmptyArray = 'W10%3D'
+
+describe('Reject On Hold Application test', () => {
+  describe('RBAC enabled', () => {
+    let crumb
+    const url = '/reject-on-hold-claim/'
+    jest.mock('../../../../app/auth')
+    let auth = { strategy: 'session-auth', credentials: { scope: [administrator] } }
+
+    beforeAll(() => {
+      jest.mock('../../../../app/config', () => ({
+        ...jest.requireActual('../../../../app/config'),
+        rbac: {
+          enabled: true
+        }
+      }))
+    })
+
+    afterAll(() => {
+      jest.resetModules()
+    })
+
+    beforeEach(async () => {
+      crumb = await getCrumbs(global.__SERVER__)
+      jest.clearAllMocks()
+    })
+
+    describe(`POST ${url} route`, () => {
+      test('returns 302 no auth', async () => {
+        const options = {
+          method: 'POST',
+          url
+        }
+        const res = await global.__SERVER__.inject(options)
+        expect(res.statusCode).toBe(302)
+      })
+
+      test('returns 403', async () => {
+        const options = {
+          method: 'POST',
+          url,
+          auth,
+          payload: {
+            reference
+          }
+        }
+        const res = await global.__SERVER__.inject(options)
+        expect(res.statusCode).toBe(403)
+        const $ = cheerio.load(res.payload)
+        expect($('h1.govuk-heading-l').text()).toEqual('403 - Forbidden')
+        expectPhaseBanner.ok($)
+      })
+
+      test('returns 403 when duplicate submission - $crumb', async () => {
+        auth = { strategy: 'session-auth', credentials: { scope: [administrator], account: { homeAccountId: 'testId', name: 'admin' } } }
+        const crumb = await getCrumbs(global.__SERVER__)
+        const options = {
+          auth,
+          method: 'POST',
+          url,
+          payload: {
+            reference,
+            rejectOnHoldClaim: 'yes',
+            page: 1,
+            crumb
+          },
+          headers: { cookie: `crumb=${crumb}` }
+        }
+        const res1 = await global.__SERVER__.inject(options)
+        expect(res1.statusCode).toBe(302)
+        const res2 = await global.__SERVER__.inject(options)
+        expect(res2.statusCode).toBe(403)
+        const $ = cheerio.load(res2.payload)
+        expectPhaseBanner.ok($)
+        expect($('.govuk-heading-l').text()).toEqual('403 - Forbidden')
+      })
+
+      test.each([
+        [authoriser, 'authoriser'],
+        [administrator, 'authoriser']
+      ])('Reject application claim processed', async (scope, role) => {
+        auth = { strategy: 'session-auth', credentials: { scope: [scope], account: { homeAccountId: 'testId', name: 'admin' } } }
+        const options = {
+          method: 'POST',
+          url,
+          auth,
+          headers: { cookie: `crumb=${crumb}` },
+          payload: {
+            reference,
+            rejectOnHoldClaim: 'yes',
+            page: 1,
+            crumb
+          }
+        }
+
+        const res = await global.__SERVER__.inject(options)
+
+        expect(res.statusCode).toBe(302)
+        expect(res.headers.location).toEqual(`/view-application/${reference}?page=1`)
+      })
+
+      test('Reject application invalid reference', async () => {
+        auth = { strategy: 'session-auth', credentials: { scope: [administrator], account: { homeAccountId: 'testId', name: 'admin' } } }
+        const options = {
+          method: 'POST',
+          url,
+          auth,
+          headers: { cookie: `crumb=${crumb}` },
+          payload: {
+            reference: 123,
+            rejectOnHoldClaim: 'yes',
+            crumb
+          }
+        }
+
+        const res = await global.__SERVER__.inject(options)
+
+        expect(res.statusCode).toBe(302)
+        expect(res.headers.location).toEqual(`/view-application/123?page=1&reject-on-hold=true&errors=${encodedEmptyArray}`)
+      })
+
+      test('Reject application claim not processed', async () => {
+        const options = {
+          method: 'POST',
+          url,
+          auth,
+          headers: { cookie: `crumb=${crumb}` },
+          payload: {
+            reference,
+            rejectOnHoldClaim: 'yes',
+            page: 1,
+            crumb
+          }
+        }
+        const res = await global.__SERVER__.inject(options)
+        expect(res.statusCode).toBe(302)
+        expect(res.headers.location).toEqual(`/view-application/${reference}?page=1`)
+      })
+    })
+
+    test('retuns 400 Bad Request', async () => {
+      const options = {
+        method: 'POST',
+        url,
+        auth,
+        headers: { cookie: `crumb=${crumb}` },
+        payload: {
+          reference,
+          page: 1,
+          crumb
+        }
+      }
+      const res = await global.__SERVER__.inject(options)
+      expect(res.statusCode).toBe(302)
+      expect(res.headers.location).toEqual(`/view-application/${reference}?page=1`)
+    })
+  })
+})
