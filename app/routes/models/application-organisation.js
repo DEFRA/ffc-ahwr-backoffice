f9d0cab90dd07df131b08d9da63d71cd7a049f93
govTechSatish <satish.chatap@capgemini.com>
2024-03-19 10:43:02 +0000

    removed org name from list

--- app/routes/models/application-organisation.js
+++ app/routes/models/application-organisation.js
@@ -1,6 +1,5 @@
 module.exports = (organisation) => {
   return [
-    { key: { text: 'Organisation Name:' }, value: { text: organisation?.orgName } },
     { key: { text: 'Name:' }, value: { text: organisation?.farmerName } },
     { key: { text: 'SBI number:' }, value: { text: organisation?.sbi } },
     { key: { text: 'Address:' }, value: { text: organisation?.address } },
