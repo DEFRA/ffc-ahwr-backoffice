144ab12c76cfb0a166eafe136fb4dcec1e305487
Afshin <147500504+Afshin-Karamifar-Capgemini@users.noreply.github.com>
2024-03-27 10:15:46 +0000

    323977 - Claim view details (#166)
    * Claim view details
    
    * version improved
    
    * Trigger pipeline
    
    * auth mocked
    
    * sonar bugs fixed
    
    * sonar bugs fixed 01
    
    * sonar bugs fixed 02

---
+++ app/constants/claim.js
@@ -0,0 +1,12 @@
+module.exports = {
+  livestockTypes: {
+    beef: 'beef',
+    dairy: 'dairy',
+    pigs: 'pigs',
+    sheep: 'sheep'
+  },
+  claimType: {
+    review: 'R',
+    endemics: 'E'
+  }
+}
