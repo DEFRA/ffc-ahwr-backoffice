279c4376445d212f883f382c4009a23f56a4afc3
Afshin-Karamifar-Capgemini <afshin.karamifar@capgemini.com>
2024-03-27 10:00:52 +0000

    sonar bugs fixed 02

--- app/routes/view-claim.js
+++ app/routes/view-claim.js
@@ -23,124 +23,36 @@ const speciesEligibleNumber = {
   sheep: '21 or more sheep'
 }
 
+const returnClaimDetailIfExist = (property, value) => property && value
 const claimSummaryDetails = (organisation, data, type) => [
-  (organisation?.name && {
-    key: { text: 'Business name' },
-    value: { html: capitalize(organisation?.name) }
-  }),
-  (data?.typeOfLivestock &&
+  (returnClaimDetailIfExist(organisation?.name, { key: { text: 'Business name' }, value: { html: capitalize(organisation?.name) } })),
+  (returnClaimDetailIfExist(data?.typeOfLivestock, { key: { text: 'Livestock' }, value: { html: capitalize([livestockTypes.pigs, livestockTypes.sheep].includes(data?.typeOfLivestock) ? data?.typeOfLivestock : `${data?.typeOfLivestock} cattle`) } })),
+  (returnClaimDetailIfExist(type, { key: { text: 'Type of review' }, value: { html: type === claimType.review ? 'Annual health and welfare review' : 'Endemic disease follow-ups' } })),
+  (returnClaimDetailIfExist(data?.dateOfVisit, { key: { text: 'Date of visit' }, value: { html: formatDate(data?.dateOfVisit) } })),
+  (returnClaimDetailIfExist(data?.dateOfTesting, { key: { text: 'Date of testing' }, value: { html: formatDate(data?.dateOfTesting) } })),
+  (returnClaimDetailIfExist(data?.speciesNumbers, { key: { text: speciesEligibleNumber[data?.typeOfLivestock] }, value: { html: capitalize(data?.speciesNumbers) } })),
+  (returnClaimDetailIfExist(data?.vetsName, { key: { text: "Vet's name" }, value: { html: capitalize(data?.vetsName) } })),
+  (returnClaimDetailIfExist(data?.vetRCVSNumber, { key: { text: "Vet's RCVS number" }, value: { html: data?.vetRCVSNumber } })),
+  (returnClaimDetailIfExist(data?.laboratoryURN, { key: { text: 'Test results URN' }, value: { html: data?.laboratoryURN } })),
+  (returnClaimDetailIfExist(data?.numberOfOralFluidSamples, { key: { text: 'Number of tests' }, value: { html: data?.numberOfOralFluidSamples } })),
+  (returnClaimDetailIfExist(data?.numberAnimalsTested, { key: { text: 'Number of animals tested' }, value: { html: data?.numberAnimalsTested } })),
+  (returnClaimDetailIfExist(data?.reviewTestResults, { key: { text: 'Review test result' }, value: { html: capitalize(data?.reviewTestResults) } })),
+  (returnClaimDetailIfExist(data?.testResults && typeof data?.testResults === 'string', { key: { text: data?.reviewTestResults ? 'Endemics test result' : 'Test result' }, value: { html: typeof data?.testResults === 'string' ? capitalize(data?.testResults) : '' } })),
+  (returnClaimDetailIfExist(data?.vetVisitsReviewTestResults, { key: { text: 'Vet Visits Review Test results' }, value: { html: capitalize(data?.vetVisitsReviewTestResults) } })),
+  (returnClaimDetailIfExist(data?.diseaseStatus, { key: { text: 'Diseases status category' }, value: { html: data?.diseaseStatus } })),
+  (returnClaimDetailIfExist(data?.numberOfSamplesTested, { key: { text: 'Samples tested' }, value: { html: data?.numberOfSamplesTested } })),
+  (returnClaimDetailIfExist(data?.herdVaccinationStatus, { key: { text: 'Herd vaccination status' }, value: { html: capitalize(data?.herdVaccinationStatus) } })),
+  (returnClaimDetailIfExist(data?.sheepEndemicsPackage, { key: { text: 'Endemics package' }, value: { html: capitalize(data?.sheepEndemicsPackage) } })),
+  (data?.biosecurity && type === claimType.endemics && [livestockTypes.pigs, livestockTypes.beef, livestockTypes.dairy].includes(data?.typeOfLivestock) &&
     {
-      key: { text: 'Livestock' },
-      value: { html: capitalize([livestockTypes.pigs, livestockTypes.sheep].includes(data?.typeOfLivestock) ? data?.typeOfLivestock : `${data?.typeOfLivestock} cattle`) }
+      key: { text: 'Biosecurity assessment' },
+      value: {
+        html:
+        data?.typeOfLivestock === livestockTypes.pigs
+          ? capitalize(`${data?.biosecurity?.biosecurity}, Assessment percentage: ${data?.biosecurity?.assessmentPercentage}%`)
+          : capitalize(data?.biosecurity)
+      }
     }
-  ),
-  (type &&
-    {
-      key: { text: 'Type of review' },
-      value: { html: type === claimType.review ? 'Annual health and welfare review' : 'Endemic disease follow-ups' }
-    }
-  ),
-  (data?.dateOfVisit &&
-    {
-      key: { text: 'Date of visit' },
-      value: { html: formatDate(data?.dateOfVisit) }
-    }
-  ),
-  (data?.dateOfTesting &&
-    {
-      key: { text: 'Date of testing' },
-      value: { html: formatDate(data?.dateOfTesting) }
-    }
-  ),
-  (data?.speciesNumbers &&
-    {
-      key: { text: speciesEligibleNumber[data?.typeOfLivestock] },
-      value: { html: capitalize(data?.speciesNumbers) }
-    }
-  ),
-  (data?.vetsName &&
-    {
-      key: { text: "Vet's name" },
-      value: { html: capitalize(data?.vetsName) }
-    }
-  ),
-  (data?.vetRCVSNumber &&
-    {
-      key: { text: "Vet's RCVS number" },
-      value: { html: data?.vetRCVSNumber }
-    }
-  ),
-  (data?.laboratoryURN &&
-    {
-      key: { text: 'Test results URN' },
-      value: { html: data?.laboratoryURN }
-    }
-  ),
-  (data?.numberOfOralFluidSamples &&
-    {
-      key: { text: 'Number of tests' },
-      value: { html: data?.numberOfOralFluidSamples }
-    }
-  ),
-  (data?.numberAnimalsTested &&
-    {
-      key: { text: 'Number of animals tested' },
-      value: { html: data?.numberAnimalsTested }
-    }
-  ),
-  (data?.reviewTestResults &&
-    {
-      key: { text: 'Review test result' },
-      value: { html: capitalize(data?.reviewTestResults) }
-    }
-  ),
-  (data?.testResults && typeof data?.testResults === 'string' &&
-    {
-      key: { text: data?.reviewTestResults ? 'Endemics test result' : 'Test result' },
-      value: { html: capitalize(data?.testResults) }
-    }
-  ),
-  (data?.vetVisitsReviewTestResults &&
-    {
-      key: { text: 'Vet Visits Review Test results' },
-      value: { html: capitalize(data?.vetVisitsReviewTestResults) }
-    }
-  ),
-  (data?.diseaseStatus &&
-    {
-      key: { text: 'Diseases status category' },
-      value: { html: data?.diseaseStatus }
-    }
-  ),
-  (data?.numberOfSamplesTested &&
-    {
-      key: { text: 'Samples tested' },
-      value: { html: data?.numberOfSamplesTested }
-    }
-  ),
-  (data?.herdVaccinationStatus &&
-    {
-      key: { text: 'Herd vaccination status' },
-      value: { html: capitalize(data?.herdVaccinationStatus) }
-    }
-  ),
-  (data?.sheepEndemicsPackage &&
-    {
-      key: { text: 'Endemics package' },
-      value: { html: capitalize(data?.sheepEndemicsPackage) }
-    }
-  ),
-  (type === claimType.endemics && [livestockTypes.pigs, livestockTypes.beef, livestockTypes.dairy].includes(data?.typeOfLivestock) &&
-
-        {
-          key: { text: 'Biosecurity assessment' },
-          value: {
-            html:
-            data?.typeOfLivestock === livestockTypes.pigs
-              ? capitalize(`${data?.biosecurity?.biosecurity}, Assessment percentage: ${data?.biosecurity?.assessmentPercentage}%`)
-              : capitalize(data?.biosecurity)
-          }
-        }
   ),
   ...(data?.typeOfLivestock === livestockTypes.sheep && type === claimType.endemics && typeof data?.testResults === 'object' && data?.testResults?.length
     ? (data?.testResults || []).map((sheepTest, index) => {
