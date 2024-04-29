9c0b04b53fe7d427e8f41b3f11ead80c80c585e1
capJuan <juan.ortega-contreras@capgemini.com>
2024-04-29 09:43:02 +0100

    move behind endemics flag (#177)
    * move bihind endemics flag
    
    * update version
    
    * test endemics flag
    
    * add mock config
    
    * update test

--- test/integration/narrow/routes/models/application-list.test.js
+++ test/integration/narrow/routes/models/application-list.test.js
@@ -4,29 +4,58 @@ const applicationData = require('.././../../../data/applications.json')
 const { getApplications } = require('../../../../../app/api/applications')
 jest.mock('../../../../../app/api/applications')
 
+const { setEndemicsEnabled } = require('../../../../mocks/config')
+
 describe('Application-list model test', () => {
-  test.each([
-    { n: 0, field: 'Reference', direction: 'DESC' },
-    { n: 0, field: 'Reference', direction: 'ASC' },
-    { n: 1, field: 'Organisation', direction: 'DESC' },
-    { n: 1, field: 'Organisation', direction: 'ASC' },
-    { n: 2, field: 'SBI', direction: 'DESC' },
-    { n: 2, field: 'SBI', direction: 'ASC' },
-    { n: 3, field: 'Apply date', direction: 'DESC' },
-    { n: 3, field: 'Apply date', direction: 'ASC' },
-    { n: 4, field: 'Status', direction: 'DESC' },
-    { n: 4, field: 'Status', direction: 'ASC' }
-  ])('getApplicationTableHeader $field $direction', async ({ n, field, direction }) => {
-    const sortField = { field: field, direction: direction }
-    const ariaSort = direction === 'DESC' ? 'descending' : 'ascending'
-    const res = getApplicationTableHeader(sortField)
-    expect(res).not.toBeNull()
-    expect(res[n].attributes['aria-sort']).toEqual(ariaSort)
+  describe('endemics enable true', () => {
+    beforeEach(() => {
+      setEndemicsEnabled(true)
+    })
+    test.each([
+      { n: 0, field: 'Reference', direction: 'DESC' },
+      { n: 0, field: 'Reference', direction: 'ASC' },
+      { n: 1, field: 'Organisation', direction: 'DESC' },
+      { n: 1, field: 'Organisation', direction: 'ASC' },
+      { n: 2, field: 'SBI', direction: 'DESC' },
+      { n: 2, field: 'SBI', direction: 'ASC' },
+      { n: 3, field: 'Apply date', direction: 'DESC' },
+      { n: 3, field: 'Apply date', direction: 'ASC' },
+      { n: 4, field: 'Status', direction: 'DESC' },
+      { n: 4, field: 'Status', direction: 'ASC' }
+    ])('getApplicationTableHeader $field $direction', async ({ n, field, direction }) => {
+      const sortField = { field: field, direction: direction }
+      const ariaSort = direction === 'DESC' ? 'descending' : 'ascending'
+      const res = getApplicationTableHeader(sortField)
+      expect(res).not.toBeNull()
+      expect(res[n].attributes['aria-sort']).toEqual(ariaSort)
+    })
+  })
+  describe('endemics enable true', () => {
+    beforeEach(() => {
+      setEndemicsEnabled(false)
+    })
+    test.each([
+      { n: 0, field: 'Reference', direction: 'DESC' },
+      { n: 0, field: 'Reference', direction: 'ASC' },
+      { n: 1, field: 'Organisation', direction: 'DESC' },
+      { n: 1, field: 'Organisation', direction: 'ASC' },
+      { n: 2, field: 'SBI', direction: 'DESC' },
+      { n: 2, field: 'SBI', direction: 'ASC' },
+      { n: 3, field: 'Apply date', direction: 'DESC' },
+      { n: 3, field: 'Apply date', direction: 'ASC' },
+      { n: 4, field: 'Status', direction: 'DESC' },
+      { n: 4, field: 'Status', direction: 'ASC' }
+    ])('getApplicationTableHeader $field $direction', async ({ n, field, direction }) => {
+      const sortField = { field: field, direction: direction }
+      const res = getApplicationTableHeader(sortField)
+      expect(res).not.toBeNull()
+    })
   })
 })
 
 describe('Application-list createModel', () => {
   beforeAll(() => {
+    setEndemicsEnabled(true)
     getApplications.mockImplementation(() => applicationData)
   })
 
