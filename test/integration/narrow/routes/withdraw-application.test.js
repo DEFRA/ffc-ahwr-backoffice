93ebafeea6d7bbad66b8933bd133c019b5b39e82
Alex White <alex.a.white@capgemini.com>
2024-05-22 14:41:09 +0100

    Fix test

--- test/integration/narrow/routes/withdraw-application.test.js
+++ test/integration/narrow/routes/withdraw-application.test.js
@@ -32,9 +32,9 @@ describe('Withdraw Application tests when ecndemis flag is On', () => {
       expect(res.statusCode).toBe(302)
     })
 
-    test('returns 403 when scope is not administrator', async () => {
+    test('returns 403 when scope is not administrator, recommender or authoriser', async () => {
       setEndemicsEnabled(true)
-      const auth = { strategy: 'session-auth', credentials: { scope: [processor, user, recommender, authoriser] } }
+      const auth = { strategy: 'session-auth', credentials: { scope: [processor, user] } }
       const options = {
         method: 'POST',
         url,
