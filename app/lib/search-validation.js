52b7299db7d364c6ad6a6bc91ac714cc4963fd93
Afshin-Karamifar-Capgemini <afshin.karamifar@capgemini.com>
2024-07-19 15:47:22 +0100

    another sonar bug fixed

--- app/lib/search-validation.js
+++ app/lib/search-validation.js
@@ -1,7 +1,7 @@
 const regexChecker = require('../../app/routes/utils/regex-checker')
 
 const refRegEx = /^(IAHW|AHWR|REPI|RESH|REBC|REDC|FUPI|FUSH|FUBC|FUDC)-[A-Z0-9]{4}-[A-Z0-9]{4}$/i
-const dateRegEx = /^(0[1-9]|[12][0-9]|3[01])[- /.](0[1-9]|1[012])[- /.](19|20)\d\d$/ // DD/MM/YYYY
+const dateRegEx = /^(0[1-9]|[12]\d|3[01])[- /.](0[1-9]|1[012])[- /.](19|20)\d\d$/ // DD/MM/YYYY
 const validStatus = [
   'agreed',
   'applied',
