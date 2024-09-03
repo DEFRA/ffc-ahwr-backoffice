99ca2d4356e4d9a028ea19c431df2e51dbefefbe
takano <tosin.akano@capgemini.com>
2024-09-03 10:48:12 +0100

    addthe needed fix

--- test/integration/narrow/routes/view-claim.test.js
+++ test/integration/narrow/routes/view-claim.test.js
@@ -433,7 +433,7 @@ describe('View claim test', () => {
           value: 'Yes, Assessment percentage: 100%'
         }
       ]
-      for (let i = 17; i < 24; i++) {
+      for (let i = 18; i < 24; i++) {
         expect($('.govuk-summary-list__key').eq(i).text()).toMatch(
           content[i].key
         )
