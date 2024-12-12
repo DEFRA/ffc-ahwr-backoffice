d1d619fb7a09d8fba527a848e0d8cd85411bca79
Rob Catton <robmcatton@gmail.com>
2024-12-12 15:26:01 +0000

    Putting back in the breaking version of msal-node, along with some debugging.

--- app/config/auth.js
+++ app/config/auth.js
@@ -9,7 +9,7 @@ const schema = Joi.object({
 })
 
 const config = {
-  enabled: process.env.AADAR_ENABLED,
+  enabled: process.env.AADAR_ENABLED === 'true',
   clientSecret: process.env.AADAR_CLIENT_SECRET,
   clientId: process.env.AADAR_CLIENT_ID,
   authority: `https://login.microsoftonline.com/${process.env.AADAR_TENANT_ID}`,
