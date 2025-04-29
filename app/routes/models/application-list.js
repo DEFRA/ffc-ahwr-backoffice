60b8cc80052dc352ec00b4e7cbde520550f55b1f
RobCatton <41433657+RobCatton@users.noreply.github.com>
2025-04-29 13:36:47 +0100

    (NO-JIRA): Fixing flag table headers for screen readers (#284)

--- app/routes/models/application-list.js
+++ app/routes/models/application-list.js
@@ -39,7 +39,7 @@ const getApplicationTableHeader = (sortField) => {
         },
       },
       {
-        html: `<span aria-hidden="true" role="img">Flagged ${FLAG_EMOJI}</span>`,
+        html: `<span>Flagged ${FLAG_EMOJI}</span>`,
       },
       {
         text: "Organisation",
@@ -121,10 +121,7 @@ async function createModel(request, page) {
           attributes: { "data-sort-value": `${app.reference}` },
         },
         {
-          html:
-            app.flags.length > 0
-              ? `<span aria-hidden="true" role="img">Yes ${FLAG_EMOJI}</span>`
-              : "",
+          html: app.flags.length > 0 ? `<span>Yes ${FLAG_EMOJI}</span>` : "",
         },
         {
           text: app.data?.organisation?.name,
