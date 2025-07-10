4760c0a92a1f8e5ad38c7c13484cf7aa3eb90ea7
aaroncarroll82 <20041831+aaroncarroll82@users.noreply.github.com>
2025-07-10 13:42:46 +0100

    fix(AHWR-380): Even more debug

--- app/routes/home.js
+++ app/routes/home.js
@@ -6,9 +6,22 @@ export const homeRoute = {
   method: "GET",
   path: "/",
   options: {
-    auth: { scope: [administrator, processor, user, recommender, authoriser] },
-    handler: async (_, h) => {
-      console.log('made it into the home handler')
+    // auth: { scope: [administrator, processor, user, recommender, authoriser] },
+    auth: false,
+    handler: async (request, h) => {
+      const isAuthenticated = request.auth.isAuthenticated;
+
+      if (!isAuthenticated) {
+        return h.redirect("/login");
+      }
+
+      // Optionally check scope here
+      const scope = request.auth.credentials.scope;
+      console.log(scope);
+      if (!['administrator', 'processor', 'user', 'recommender', 'authoriser'].some(s => scope.includes(s))) {
+        return h.response("Forbidden").code(403);
+      }
+
       return h.redirect("/claims");
     },
   },
