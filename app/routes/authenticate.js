1531f30ccb563e0f300d292290ec2c06fe27add4
aaroncarroll82 <20041831+aaroncarroll82@users.noreply.github.com>
2025-07-10 11:49:45 +0100

    fix(AHWR-380): Debug for the issue

--- app/routes/authenticate.js
+++ app/routes/authenticate.js
@@ -9,9 +9,11 @@ export const authenticateRoute = {
   handler: async (request, h) => {
     try {
       await auth.authenticate(request.query.code, request.cookieAuth);
+      console.log('In authenticateRoute handler - about to redirect');
       return h.redirect("/");
     } catch (err) {
       request.logger.setBindings({ err });
+      request.logger.error(err.message);
     }
 
     return h.view("error-pages/500").code(500);
