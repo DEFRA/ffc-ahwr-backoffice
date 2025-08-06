2f8044be851e3f04280b0b9198f23322385784ea
rossjarvisesynergy <ross.jarvis@esynergy.co.uk>
2025-08-06 15:53:36 +0100

    increased test coverage

--- test/integration/narrow/app/server.test.js
+++ test/integration/narrow/app/server.test.js
@@ -1,7 +1,17 @@
 import { createServer } from "../../../../app/server.js";
 
+jest.mock('../../../../app/crons/process-on-hold/scheduler.js')
+jest.mock('../../../../app/crons/data-redaction/scheduler.js')
+
 describe("Server test", () => {
   test("createServer returns server", async () => {
+    process.env.NODE_ENV = 'production'    
+    const server = await createServer();
+    expect(server).toBeDefined();
+  });
+
+  test("createServer returns server when env is test", async () => {
+    process.env.NODE_ENV = 'test'
     const server = await createServer();
     expect(server).toBeDefined();
   });
